819c19804261fa2ec5d118f6447bee6bebfce31f
sangkeerthana70 <sangkeerthana70@gmail.com>
2018-07-22 07:42:12 -0400

    Testing if commits are pushed to the New repo

---
+++ SeleniumTwo/ExecLib.cs
@@ -0,0 +1,42 @@
+﻿
+using ExcelDataReader;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTwo
+{
+    class ExecLib
+    {
+        public static DataTable ExcelToDataTable(string fileName)
+        {
+            //open file and returns as Stream
+            FileStream stream = File.Open(fileName, FileMode.Open, FileAccess.Read);
+            //Createopenxmlreader via ExcelReaderFactory
+            ExcelDataReader.IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream); //.xlsx
+            //'IsFirstRowAsColumnNames' is not recognized anymore replaced the following code to store  DataSet Result in a variable.                                                                              //Set the First Row as Column Name
+            //excelReader.IsFirstRowAsColumnNames = true;
+            //Return as DataSet
+            DataSet result = excelReader.AsDataSet(new ExcelDataSetConfiguration()
+            {
+                ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true
+                }
+            });
+            
+             result = excelReader.AsDataSet();
+            //Get all the Tables
+            DataTableCollection table = result.Tables;
+            //Store it in DataTable
+            DataTable resultTable = table["Sheet1"];
+
+            //return
+            return resultTable;
+        }   
+    }
+}
